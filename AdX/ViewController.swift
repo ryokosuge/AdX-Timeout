60efc156c57bb7711be61d7056074e8313f92945
ryokosuge <ryo.kosuge@gmail.com>
2019-05-08 16:45:35 +0900

    Initial Commit

---
+++ AdX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdX
+//
+//  Created by RyoKosuge on 2019/05/08.
+//  Copyright © 2019年 Ryo Kosuge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
